c36d0c4dab0e429fee8fa98669ff077742999d8b
Michael Wendell <mwendell@kwyjibo.com>
2025-08-30 19:21:56 -0400

    working on the front-end

--- afa-most-popular-public.php
+++ afa-most-popular-public.php
@@ -1,6 +1,19 @@
 <?php
 function afa_render_most_popular_shortcode( $atts ) {
 
+	$defaults = array(
+		'count' => 3,
+	);
+
+    $attributes = shortcode_atts( $default, $atts );
+
+	echo afa_most_popular_render( $attributes['count'] );
+
+}
+add_shortcode( 'afa_most_popular', 'afa_render_most_popular_shortcode' );
+
+function afa_most_popular_render( $count = 3, $echo = false ) {
+
 	$popular = get_option( 'afa_most_popular_posts', array() );
 	$allowed_types = get_option( 'afa_most_popular_post_types', array( 'post', 'article' ) );
 
@@ -8,14 +21,9 @@ function afa_render_most_popular_shortcode( $atts ) {
 		return;
 	}
 
-	wp_enqueue_style( 'afa-most-popular-shortcode-css', 'afa-most-popular.css' );
+	$output = "<div class='items grid-container background-gray trending-stories'><h3 class='sidebar-title col-sm-12'>Most popular</h3>";
 
-	ob_start();
-	echo "<div class='items grid-container background-gray trending-stories'><h3 class='sidebar-title col-sm-12'>Most popular</h3>";
-
-
-
-
+	$i = 0;
 	foreach ( $popular as $post ) {
 
 		if ( ! in_array( $post['post_type'], $allowed_types, true ) ) {
@@ -30,17 +38,30 @@ function afa_render_most_popular_shortcode( $atts ) {
 			continue;
 		}
 
-		echo "<div class='item article-trending-stories grid-container'><div class='col-sm-2 col-lg-4 col-no-pad col-no-margin image-wrapper'>";
-		echo "<a href='{$url}' title='{$title}'><img width='150' height='150' src='{$image}' class='attachment-thumbnail size-thumbnail wp-post-image' alt='' decoding='async' loading='lazy'></a></div>";
-		echo "<div class='col-sm-10 col-lg-8 col-no-pad col-no-margin'>";
-		echo "<h4 class='post-title'><a href='{$url}' rel='bookmark' title='{$title}'>{$title}</a></h4>";
-		echo "</div></div>";
+		$i++;
+		$output .= "<div class='item article-trending-stories grid-container'><div class='col-sm-2 col-lg-4 col-no-pad col-no-margin image-wrapper'>";
+		$output .= "<a href='{$url}' title='{$title}'><img width='150' height='150' src='{$image}' class='attachment-thumbnail size-thumbnail wp-post-image' alt='' decoding='async' loading='lazy'></a></div>";
+		$output .= "<div class='col-sm-10 col-lg-8 col-no-pad col-no-margin'>";
+		$output .= "<h4 class='post-title'><a href='{$url}' rel='bookmark' title='{$title}'>{$title}</a></h4>";
+		$output .= "</div></div>";
+
+		if ( $i == $count ) {
+			break;
+		}
+
 	}
 
-	echo '</div>';
-	return ob_get_clean();
+	$output .=  '</div>';
+
+	if ( $echo ) {
+		echo $output;
+	} else {
+		return $output;
+	}
+
+
 }
-add_shortcode( 'afa_most_popular', 'afa_render_most_popular_shortcode' );
+
 
 /*
  <!--Array
